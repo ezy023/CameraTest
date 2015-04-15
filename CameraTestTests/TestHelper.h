d9229511b63e2bc11d0043e00e83f4a10df1792e
ezy023 <ezy023@gmail.com>
2015-04-15 18:28:28 -0500

    Add OCMockito to project. Create TestHelper.h for imports

---
+++ CameraTestTests/TestHelper.h
@@ -0,0 +1,18 @@
+//
+//  TestHelper.h
+//  CameraTest
+//
+//  Created by Erik Allar on 4/15/15.
+//  Copyright (c) 2015 Erik Allar. All rights reserved.
+//
+
+#ifndef CameraTest_TestHelper_h
+#define CameraTest_TestHelper_h
+
+#define HC_SHORTHAND
+#import <OCHamcrestIOS/OCHamcrestIOS.h>
+
+#define MOCKITO_SHORTHAND
+#import <OCMockitoIOS/OCMockitoIOS.h>
+
+#endif
